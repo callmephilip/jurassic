81e65e14e8d686d7cffebde12b288e937fdeaac0
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2024-12-12 11:46:59 +0000

    move things around, refactor

--- bin/export.ts
+++ bin/export.ts
@@ -1,18 +1,19 @@
-import { exportNb, getConfig } from "../jurassic/export.ts";
+import { exportNb } from "jurassic/export.ts";
+import { getConfig } from "jurassic/config.ts";
 
 if (import.meta.main) {
   const args = Deno.args;
 
   if (args.length !== 1) {
     console.error(
-      "Usage: deno run jsr:@jurassic/jurassic/export root_directory",
+      "Usage: deno run jsr:@jurassic/jurassic/export root_directory"
     );
     Deno.exit(1);
   }
 
   const config = await getConfig();
   await Deno.stdout.write(
-    new TextEncoder().encode(`Using config from: ${config.configPath}\n`),
+    new TextEncoder().encode(`Using config from: ${config.configPath}\n`)
   );
   await exportNb(args[0], config);
 }
