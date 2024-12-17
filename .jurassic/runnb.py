40ca741e3a0b24e297b8168df0aa8c54ec053e6b
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2024-12-17 15:42:31 +0000

    retry nb runs

--- .jurassic/runnb.py
+++ .jurassic/runnb.py
@@ -3,7 +3,7 @@
 import sys, nbformat, argparse
 from pathlib import Path
 from nbclient import NotebookClient
-
+from retry import retry
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Execute Jupyter notebook')
@@ -21,11 +21,16 @@ def parse_args():
     return parser.parse_args()
 
 
+@retry(tries=3, delay=2)
+def run(notebook, nbs_path):
+    nb = nbformat.read(notebook, as_version=4)
+    NotebookClient(nb, timeout=600, kernel_name='deno', resources={'metadata': {'path': nbs_path}}).execute()
+    nbformat.write(nb, notebook)
+
 if __name__ == "__main__":
     args = parse_args()
     print(f"Using Python from: {sys.executable}")
     print(f"With sys.path: {sys.path[0]}")
+    run(args.notebook, args.nbs_path)
     
-    nb = nbformat.read(args.notebook, as_version=4)
-    NotebookClient(nb, timeout=600, kernel_name='deno', resources={'metadata': {'path': args.nbs_path}}).execute()
-    nbformat.write(nb, args.notebook)
+    
