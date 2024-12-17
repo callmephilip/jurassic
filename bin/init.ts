1b733a01a3f1810c78bc065346a0c65fed059e7d
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2024-12-17 10:55:28 +0000

    0.1.21

--- bin/init.ts
+++ bin/init.ts
@@ -6,7 +6,7 @@ if (import.meta.main) {
 
   if (args.length !== 1) {
     console.error(
-      "Usage: deno run --reload -R -W -N jsr:@jurassic/jurassic/init project-name"
+      "Usage: deno run --reload -R -W -N jsr:@jurassic/jurassic/init project-name",
     );
     Deno.exit(1);
   }
