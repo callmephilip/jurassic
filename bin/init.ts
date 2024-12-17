ea9e244493717e86c93784e9a5bce190f5d2dbc6
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2024-12-17 09:18:49 +0000

    add init script placeholder

---
+++ bin/init.ts
@@ -0,0 +1,12 @@
+if (import.meta.main) {
+  //   const args = Deno.args;
+
+  //   if (args.length !== 1) {
+  //     console.error(
+  //       "Usage: deno run jsr:@jurassic/jurassic/export root_directory"
+  //     );
+  //     Deno.exit(1);
+  //   }
+
+  console.log("Gonna create a new project");
+}
