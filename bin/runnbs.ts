f78dd8e0e396ff940867977dee82fe1765584a7e
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2025-01-03 13:37:37 +0000

    fix retry?

--- bin/runnbs.ts
+++ bin/runnbs.ts
@@ -5,10 +5,10 @@ import path from "node:path";
 
 if (import.meta.main) {
   const config = await getConfig();
-  const results = await Promise.all(
+  await Promise.all(
     config.notebooks.map((nb: string) => {
       console.log(`Running ${nb}`);
-      const fn = () => {
+      const fn = async () => {
         const command = new Deno.Command("jupyter", {
           args: [
             "execute",
@@ -20,13 +20,14 @@ if (import.meta.main) {
         });
         const child = command.spawn();
         child.stdin.close();
-        return child.status;
+
+        const r = await child.status;
+
+        if (!r.success) {
+          throw new Error(`Failed to run notebook: ${nb}`);
+        }
       };
       return retry(fn, { maxAttempts: 3, minTimeout: 100 });
     }),
   );
-
-  if (results.filter((r) => r.code !== 0).length > 0) {
-    throw new Error("Failed to run one or more notebooks");
-  }
 }
