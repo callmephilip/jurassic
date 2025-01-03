834f8a259013da5514c191df20963e8319142f96
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2025-01-03 11:36:27 +0000

    get rid of clean.py

--- bin/clean.ts
+++ bin/clean.ts
@@ -8,15 +8,4 @@ if (import.meta.main) {
   for (const nb of config.notebooks) {
     await cleanNotebook(path.resolve(config.nbsPath, nb));
   }
-
-  // const command = new Deno.Command(".jurassic/clean.py", {
-  //   args: [config.nbsPath],
-  // });
-  // const { code, stderr, stdout } = command.outputSync();
-  // console.log(new TextDecoder().decode(stdout));
-  // console.error(new TextDecoder().decode(stderr));
-  // console.log(">>> Code", code);
-  // if (code !== 0) {
-  //   throw new Error("Failed to clean notebooks");
-  // }
 }
